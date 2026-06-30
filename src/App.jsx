27b78bf545a882ba8e85ca9f0bced09ff2d307af
Rukaiya <rukaiyaakram2@gmail.com>
2026-06-30 11:25:34 +0500

    feat(Product): complete productpage layout

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
    {/* Background */}
    <div className = "fixed inset-0 z-0">
-    <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80" />
+    <div className="absolute inset-0 bg-linear-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80" />
     <div className="absolute inset-0 backdrop-blur-sm" />
    </div>
 
--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -1,10 +1,68 @@
 import React from 'react'
+import Header from '../components/commonComponents/header/Header'
+import StatCard from '../components/commonComponents/statCards/StatCard'
+import { motion } from 'framer-motion'
+import { AlertTriangle, DollarSign, Package, TrendingUp } from "lucide-react";
+import CategoryDistributionChart from '../components/overviewPage/CategoryDistributionChart'
+import SalesTrendChart from '../components/productPage/SalesTrendChart';
+import ProductsTable from '../components/productPage/ProductsTable';
+
 
 function ProductPage() {
   return (
-    <div>
-      ProductPage
+    <div className=" flex-1 overflow-auto relative z-10">
+        {/* --------------------Header--------------- */}
+      <Header title="Products" />
+
+        {/* --------------------Stat Cards--------------- */}
+      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+        <motion.div
+          className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          >
+          <StatCard
+            name="Total Products"
+            icon={Package}
+            value="1234"
+            color="#6366F1"
+            />
+          <StatCard
+            name="Top Sellings"
+            icon={TrendingUp}
+            value="89"
+            color="#10B981"
+            />
+          <StatCard
+            name="Low Stock"
+            icon={AlertTriangle}
+            value="23"
+            color="#F59E0B"
+            />
+          <StatCard
+            name="Total Revenue"
+            icon={DollarSign}
+            value="$543,210"
+            color="#EF4444"
+            />
+        
+        </motion.div>
+
+            {/* --------------------Product Table--------------- */}
+  <ProductsTable />
+
+
+        {/* --------------------Charts--------------- */}
+				<div className='grid grid-col-1 lg:grid-cols-2 gap-8'>
+					<SalesTrendChart />
+					<CategoryDistributionChart />
+				</div>
+
+      
+      </main>
     </div>
+    
   )
 }
 
